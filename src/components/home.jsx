75c6e2f65d9ab23be949078a0eec09931a5f8f4b
Jabby Alicante <alicantejabbylumenso@gmail.com>
2026-04-10 18:34:51 +0800

    home portfolio

---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+export default function home(element) {
+  element.innerHTML = `
+    <section class="hero">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Discover my projects and skills</p>
+      <p>portfolio in progress</p>
+    </section>
+  `;
+}
