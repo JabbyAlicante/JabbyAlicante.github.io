b729e87385f25d68df5f27c6fb95d2c778f10a06
Jabby Alicante <alicantejabbylumenso@gmail.com>
2026-04-10 18:49:13 +0800

    rm

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/JabbyAlicante.github.io/', // 👈 CHANGE THIS to your repo name
+  base: '/JabbyAlicante.github.io/', 
 })
